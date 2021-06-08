538c0e83e3b788945bd178930d6e11a0799cb794
Brinton <brintonfoyreed@gmail.com>
2021-06-07 20:08:06 -0700

    commit tree 154575955f62159bd2fee2ac37491839f4841f17

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
 
+    def name=(name) #setter method as indicated by '='
+        @name = name
+    end
+
+    def name #getter method
+        @name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    def job
+        @job
+    end
+
 end
